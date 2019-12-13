a91654c7ae623f2237b8733e2552209559b65fb2
zane-uw <zane@uw.edu>
2019-12-13 12:21:21 -0800

    create wide compass features

--- src_r/risk_gpa25/001a_compass-data.R
+++ src_r/risk_gpa25/001a_compass-data.R
@@ -1,47 +1,60 @@
+rm(list = ls())
+gc()
+
 library(dbplyr)
 library(odbc)
 library(tidyverse)
 
 setwd(rstudioapi::getActiveProject())
 
-con <- dbConnect(odbc::odbc(), 'compass', UID = config::get("sdb", file = "config.yml")$uid, PWD = keyring::key_get("sdb"))
+options(tibble.print_min = 30)
 
-dbListTables(con)
+# fetch (new) data --------------------------------------------------------
 
-appt <- tbl(con, 'appointment') %>%
-  # filter(year(Date) >= 2008) %>%
-  select(ID,
-         student_no,
-         staff_id,
-         Contact_Type,
-         Date,
-         Time_In,
-         Time_Out,
-         .data$AddDropclass : .data$Notes,
-         Event_Type,
-         Source,
-         TimeDateIn,
-         TimeDateOut,
-         AutoLogOut) %>%
-  collect()
-
-dbDisconnect(con); rm(con)
-
-con <- dbConnect(odbc(), 'sqlserver01', UID = config::get('sdb', file = 'config.yml')$uid, PWD = keyring::key_get('sdb'))
-cal <- tbl(con, in_schema('EDWPresentation.sec', 'dimDate')) %>%
-  filter(CalendarYr >= 2008) %>%
-  select(CalendarDate, AcademicQtrKeyId, AcademicQtrDayNum, AcademicQtrWeekNum, AcademicQtrName, AcademicQtrCensusDayInd, AcademicYrName) %>%
-  collect() %>%
-  mutate(yr = as.numeric(AcademicQtrKeyId) %/% 10)
-
+# con <- dbConnect(odbc::odbc(), 'compass', UID = config::get("sdb", file = "config.yml")$uid, PWD = keyring::key_get("sdb"))
+#
+# dbListTables(con)
+#
+# appt <- tbl(con, 'appointment') %>%
+#   # filter(year(Date) >= 2008) %>%
+#   select(ID,
+#          student_no,
+#          staff_id,
+#          Contact_Type,
+#          Date,
+#          Time_In,
+#          Time_Out,
+#          .data$AddDropclass : .data$Notes,
+#          Event_Type,
+#          Source,
+#          TimeDateIn,
+#          TimeDateOut,
+#          AutoLogOut) %>%
+#   collect()
+#
+# dbDisconnect(con); rm(con)
+#
+# con <- dbConnect(odbc(), 'sqlserver01', UID = config::get('sdb', file = 'config.yml')$uid, PWD = keyring::key_get('sdb'))
+# cal <- tbl(con, in_schema('EDWPresentation.sec', 'dimDate')) %>%
+#   filter(CalendarYr >= 2008) %>%
+#   select(CalendarDate, AcademicQtrKeyId, AcademicQtrDayNum, AcademicQtrWeekNum, AcademicQtrName, AcademicQtrCensusDayInd, AcademicYrName) %>%
+#   collect() %>%
+#   mutate(yr = as.numeric(AcademicQtrKeyId) %/% 10)
+#
 # sid <- tbl(con, in_schema('sec', 'student_1')) %>%
 #   select(system_key, student_no) %>%
 #   collect()
-
-dbDisconnect(con); rm(con)
-
+#
+# dbDisconnect(con); rm(con)
+#
+# save(appt, cal, sid, file = 'src_r/risk_gpa25/data/compass.RData')
 
 # merge -------------------------------------------------------------------
+
+rm(list = ls())
+gc()
+
+load('src_r/risk_gpa25/data/compass.RData')
 
 dat <- appt %>%
   # mutate(Date = as.POSIXct(Date, tz = 'UTC')) %>%
@@ -79,22 +92,68 @@ adv <- dat %>% filter(dat$Contact_Type %in% c('Appointment', 'Quick Question', '
 #   geom_line()
 
 cbind(apply(adv[,8:28], 2, function(x) round(mean(x, na.rm = T), 3)))
+#
+# adv %>%
+#   # group_by(qtr.label, AcademicQtrWeekNum) %>%
+#   summarize_at(vars(AddDropclass:NoShow), sum, na.rm = T) %>%
+#   # ungroup() %>%
+#   # mutate(xvar = paste(qtr.label, AcademicQtrWeekNum, sep = '-')) %>%
+#   pivot_longer(., cols = AddDropclass:NoShow) %>%
+#   arrange(desc(value))
+#
+# adv %>%
+#   group_by(qtr.label, AcademicQtrWeekNum) %>%
+#   summarize_at(vars(AddDropclass:NoShow), sum) %>%
+#   ungroup() %>%
+#   mutate(xvar = paste(qtr.label, AcademicQtrWeekNum, sep = '-')) %>%
+#   pivot_longer(., cols = AddDropclass:NoShow) %>%
+#   ggplot(., aes(x = xvar, y = value, group = name, color = name)) +
+#   geom_line() +
+#   theme(axis.text.x = element_text(angle = 45))
+#
+# adv %>%
+#   mutate(hour = as.numeric(str_sub(Time_In, start = 1, end = 2))) %>%
+#   group_by(qtr.label, hour) %>%
+#   summarize_at(vars(AddDropclass:NoShow), sum) %>%
+#   ungroup() %>%
+#   pivot_longer(., col = AddDropclass:NoShow) %>%
+#   ggplot(., aes(x = hour, y = name, fill = value)) +
+#   geom_raster()
 
-adv %>%
-  group_by(qtr.label, AcademicQtrWeekNum) %>%
-  summarize_at(vars(AddDropclass:NoShow), sum) %>%
-  ungroup() %>%
-  mutate(xvar = paste(qtr.label, AcademicQtrWeekNum, sep = '-')) %>%
-  pivot_longer(., cols = AddDropclass:NoShow) %>%
-  ggplot(., aes(x = xvar, y = value, group = name, color = name)) +
-  geom_line() +
-  theme(axis.text.x = element_text(angle = 45))
 
-adv %>%
-  mutate(hour = as.numeric(str_sub(Time_In, start = 1, end = 2))) %>%
-  group_by(qtr.label, hour) %>%
-  summarize_at(vars(AddDropclass:NoShow), sum) %>%
+# aggregate ---------------------------------------------------------------
+
+adv.agg <- adv %>%
+  group_by(student_no, yr, qtr.num) %>%
+  summarize_at(vars(AddDropclass:NoShow), sum, na.rm = T) %>%
+  ungroup()
+ic.activity.agg <- ic %>%
+  group_by(student_no, yr, qtr.num, Contact_Type) %>%
+  summarize(n = n()) %>%
   ungroup() %>%
-  pivot_longer(., col = AddDropclass:NoShow) %>%
-  ggplot(., aes(x = hour, y = name, fill = value)) +
-  geom_raster()
+  pivot_wider(., names_from = 'Contact_Type', values_from = 'n')
+ic.activity.agg[,4:17] <- apply(ic.activity.agg[,4:17], 2, function(x) replace_na(x, 0))
+
+str_clean <- function(x){
+  x <- str_remove_all(x, '[[:punct:]]')
+  # x <- tolower(x)
+  x <- str_replace_all(x, " ", "_")
+}
+
+n <- str_clean(names(ic.activity.agg[4:ncol(ic.activity.agg)]))
+# n <- str_remove_all(n, '[[:punct:]]')
+# n <- c(names(ic.activity.agg[1:3]), n)
+names(ic.activity.agg) <- c(names(ic.activity.agg[1:3]), n)
+
+
+
+# add system_key; combine ----------------------------------------------------------
+
+compass.feats <- full_join(adv.agg, ic.activity.agg) %>%
+  filter(student_no > 1) %>%
+  mutate(yrq = (yr*10)+qtr.num) %>%
+  select(-yr, -qtr.num) %>%
+  inner_join(sid) %>%
+  select(-student_no)
+
+save(compass.feats, file = 'src_r/risk_gpa25/data/compass-features.RData')
